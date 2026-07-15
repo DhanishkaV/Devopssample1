c2d7c458fbfb603abcff4567752b6cddbf1c6500
Dhanishka Ve <todhanishka2006@gmail.com>
2026-07-15 09:23:55 +0000

    commited

---
+++ calculator2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main()
+{
+        int a,b,c;
+        cout<<"enter operand , operator 1 and operator 2:";
+        cin >> c>>a>>b;
+        switch(c)
+        {
+                case '+':
+                        cout<<(a+b);
+                        break;
+                case '-':
+                        cout<<(a-b);
+                        break;
+		default:
+	}
+}
