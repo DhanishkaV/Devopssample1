a9ae3fc00aa0009f1966aee500b9b553f7665795
DhanishkaV <todhanishka2006@gmail.com>
2026-08-19 13:59:28 +0530

    comitted

--- calculator.cpp
+++ calculator.cpp
@@ -2,7 +2,7 @@
 int main()
 {
 	int a,b,c;
-	cout<<"this is new code :enter operand , operator 1 and operator 2:";
+	cout<<"hii this is new code :enter operand , operator 1 and operator 2:";
 	cin >> c>>a>>b;
 	switch(c)
 	{
